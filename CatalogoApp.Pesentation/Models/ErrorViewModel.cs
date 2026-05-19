c865499bd61adce7359ccaf3a26d25dde2003999
Dylanmosqueda <dylan.mosqueda@tecdesoftware.edu.mx>
2026-05-19 08:18:22 -0600

    CREATE: Modelo de capas

---
+++ CatalogoApp.Pesentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatalogoApp.Pesentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
